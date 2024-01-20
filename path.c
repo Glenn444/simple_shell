a0262181e78a40d2fdceba8a8c08ae6c8c2bf731
Glenn444 <gmark1586@gmail.com>
2024-01-20 19:56:54 +0000

    simple shell

--- path.c
+++ path.c
@@ -15,20 +15,3 @@ char *getenv_custom(const char *name)
 	}
 	return (NULL);
 }
-/**
- * main - prints the environment
- *
- * Return: Always 0.
- */
-int main(int ac, char **av, char **env)
-{
-    unsigned int i;
-
-    i = 0;
-    while (env[i] != NULL)
-    {
-        printf("%s\n", env[i]);
-        i++;
-    }
-    return (0);
-}
